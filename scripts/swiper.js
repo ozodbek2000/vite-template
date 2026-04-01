b167cd82c81ad6689008044222e32276950caefd
ozodbek2000 <ozodbek.boriboyev2000@gmail.com>
2026-04-01 17:24:45 +0500

    added 3 sections

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -11,8 +11,28 @@ var collectionSwiper = new Swiper(".collection", {
 
 var discountSwiper = new Swiper(".discount", {
     spaceBetween: 16,
-  pagination: {
-      el: ".discount-pagination",
-      clickable: true,
-  },
+    pagination: {
+        el: ".discount-pagination",
+        clickable: true,
+    },
+});
+
+var discountSwiper = new Swiper(".sofa", {
+    spaceBetween: 16,
+    slidesPerView: 1.3  ,
+    scrollbar: {
+        el: document.querySelector(".sofa-scrollbar"),
+        // hide: true,
+        draggable: true,
+    },
+    breakpoints: {
+        767: {
+            spaceBetween: 20,
+            slidesPerView: 3,
+            grid: {
+                rows: 2,
+                fill: "row",
+            },
+        },
+    },
 });
